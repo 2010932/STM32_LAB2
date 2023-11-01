1f0f489406f5aefc292ac161d578abd79139bff1
PINKF\Admin <binh.nguyengiabinh@hcmut.edu.vn>
2023-11-01 22:40:33 +0700

    init for EX9

---
+++ LAB2_EX9/Core/Inc/software_timer.h
@@ -0,0 +1,17 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 12, 2023
+ *      Author: Admin
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag, timer2_flag,timer3_flag;
+void setTimer1(int duration);
+void setTimer2(int duration);
+void setTimer3(int duration);
+void timeRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
